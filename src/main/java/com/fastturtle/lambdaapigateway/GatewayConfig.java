357bd16c3d468e98818fd464ec495a5c3549acfe
Divya Gupta <divygupta0319@gmail.com>
2025-01-31 17:11:35 +0530

    Still working on resolving blank response coming in Gateway call

--- src/main/java/com/fastturtle/lambdaapigateway/GatewayConfig.java
+++ src/main/java/com/fastturtle/lambdaapigateway/GatewayConfig.java
@@ -3,7 +3,6 @@ package com.fastturtle.lambdaapigateway;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fastturtle.lambdaapigateway.models.Request;
-import com.fastturtle.lambdaapigateway.models.Response;
 import com.fastturtle.lambdaapigateway.services.LambdaService;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
@@ -20,26 +19,28 @@ public class GatewayConfig {
         return builder.routes()
                 .route("lambda_tts_route", r -> r
                         .path("/callTextToAudioLambda")
-                        .filters(f -> f.modifyRequestBody(String.class, String.class,
+                        .filters(f -> f.addResponseHeader("Content-Type", "application/json")
+                                .modifyRequestBody(String.class, String.class,
                                         (exchange, requestBodyStr) -> {
                                             try {
                                                 Request request = objectMapper.readValue(requestBodyStr, Request.class);
                                                 String lambdaResponse = lambdaService.invokeLambdaForTTSUsingPolly(request);
+
+                                                System.out.println("Lambda Response in Gateway: " + lambdaResponse);
+
                                                 // Return the Lambda response (which is a String)
                                                 return Mono.just(lambdaResponse);
                                             } catch (JsonProcessingException e) {
                                                 return Mono.error(new RuntimeException("Error parsing request JSON", e));
                                             }
                                         })
-                                .modifyResponseBody(String.class, Response.class,
+                                .modifyResponseBody(String.class, String.class,
                                         (exchange, responseBody) -> {
-                                            try {
-                                                return Mono.just(objectMapper.readValue(responseBody, Response.class));
-                                            } catch (JsonProcessingException e) {
-                                                return Mono.error(new RuntimeException("Error parsing Lambda response", e));
-                                            }
+                                            System.out.println("Modifying Response in Gateway: " + responseBody);
+                                            return Mono.just(responseBody);
                                         }))
-                        .uri("no://op"))
+
+                        .uri("forward:/dummy"))
                 .build();
     }
 }

faf5d86a3c21cf83c5c14e6b3f9c84f20b740f46
Divya Gupta <divygupta0319@gmail.com>
2025-01-31 11:11:54 +0530

    Initial commit with Reactor gateway and Lambda dependency added

---
+++ src/main/java/com/fastturtle/lambdaapigateway/LambdaApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fastturtle.lambdaapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LambdaApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LambdaApiGatewayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastturtle/lambdaapigateway/LambdaApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastturtle.lambdaapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LambdaApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

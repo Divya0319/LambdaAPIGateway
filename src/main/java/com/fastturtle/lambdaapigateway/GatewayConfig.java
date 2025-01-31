a0f4710282f3b5629db02dec8bae4f695d4efea0
divya0319 <divygupta0319@gmail.com>
2025-01-31 20:22:54 +0530

    Added API gateway config for routing to callTextToAudioLambda API in 8081 server

--- src/main/java/com/fastturtle/lambdaapigateway/GatewayConfig.java
+++ src/main/java/com/fastturtle/lambdaapigateway/GatewayConfig.java
@@ -1,9 +1,5 @@
 package com.fastturtle.lambdaapigateway;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fastturtle.lambdaapigateway.models.Request;
-import com.fastturtle.lambdaapigateway.services.LambdaService;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -15,32 +11,13 @@ public class GatewayConfig {
 
 
     @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder, LambdaService lambdaService, ObjectMapper objectMapper) {
+    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
-                .route("lambda_tts_route", r -> r
+                .route(r -> r
                         .path("/callTextToAudioLambda")
-                        .filters(f -> f.addResponseHeader("Content-Type", "application/json")
-                                .modifyRequestBody(String.class, String.class,
-                                        (exchange, requestBodyStr) -> {
-                                            try {
-                                                Request request = objectMapper.readValue(requestBodyStr, Request.class);
-                                                String lambdaResponse = lambdaService.invokeLambdaForTTSUsingPolly(request);
-
-                                                System.out.println("Lambda Response in Gateway: " + lambdaResponse);
-
-                                                // Return the Lambda response (which is a String)
-                                                return Mono.just(lambdaResponse);
-                                            } catch (JsonProcessingException e) {
-                                                return Mono.error(new RuntimeException("Error parsing request JSON", e));
-                                            }
-                                        })
-                                .modifyResponseBody(String.class, String.class,
-                                        (exchange, responseBody) -> {
-                                            System.out.println("Modifying Response in Gateway: " + responseBody);
-                                            return Mono.just(responseBody);
-                                        }))
-
-                        .uri("forward:/dummy"))
+                        .filters(f -> f.stripPrefix(0)
+                                .preserveHostHeader())
+                        .uri("http://localhost:8081"))
                 .build();
     }
 }
--- src/main/java/com/fastturtle/lambdaapigateway/models/Status.java
+++
@@ -1,5 +0,0 @@
-package com.fastturtle.lambdaapigateway.models;
-
-public enum Status {
-    SUCCESS, FAILURE
-}
